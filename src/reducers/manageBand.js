2026037e5a116d416a0aea56b4ed436b7e3ee5ad
Vincent Tran <vincentuong@yahoo.com>
2022-04-04 21:50:03 -0500

    commit tree d2a9fd832d0e39fdc1cfb593c467b95bd2bba96f

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,11 +1,14 @@
 export default function manageBand(state = {
-  bands: [],
+  bands: []
 }, action) {
-  switch(action.type) {
-    case "ADD_BAND":
-      return {...state, bands: state.bands.concat(action.name)}
+  switch (action.type) {
+    case 'ADD_BAND':
+      const band = { id: Math.random(), name: action.name }
+      return { ...state, bands: [...state.bands, band] }
+    case 'DELETE_BAND':
+      const bands = state.bands.filter(band => band.id !== action.id);
+      return { bands };
     default:
-      return state
-  
+      return state;
   }
 };
